c1498f16b7e43be64d3c0c95d305e3e076cdcf07
LEEHYEYEONG <76934619+LEEHYEYEONG@users.noreply.github.com>
2021-11-28 19:40:29 +0900

    Add files via upload

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,7 @@ const API_KEY = "de3e151406c2df49e74c2084ef243338";
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
   fetch(url).then(response => response.json()).then(data => {
     const weather = document.querySelector("#weather span:first-child");
     const city = document.querySelector("#weather span:last-child");
